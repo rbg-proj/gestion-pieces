85a0b82d46ee5458b0da6feca98f227b40f9f5c0
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-11-22 00:13:23 +0100

    Updated Sidebar.tsx

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -32,7 +32,6 @@ const Sidebar: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
     { name: 'Rapport Ventes', path: '/orders', icon: <ShoppingCart size={20} />, roles: ['admin', 'manager', 'employee'] },
     { name: 'Autres Rapports', path: '/reports', icon: <BarChartBig size={20} />, roles: ['admin', 'manager'] },
     { name: 'Historique Stock', path: '/stock-history', icon: <PieChart size={20} />, roles: ['admin', 'manager'] },
-  ];
   { name: 'Taux de change', path: '/exchangeRates', icon: <Rate size={20} />, roles: ['admin', 'manager'] },
   ];
 
