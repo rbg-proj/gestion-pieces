2866f5022cd3872abec768800a81a33578a8dade
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-11-21 23:39:25 +0100

    Updated capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.pts.gesvente',
-  appName: 'Gestion des ventes',
+  appName: 'RBG POS',
   webDir: 'dist'
 };
 
