d4b3b3e10869b8cebe36d2ad78fa7744743dc1e6
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-11-21 11:35:19 +0100

    Updated Sales.tsx

--- src/pages/Sales.tsx
+++ src/pages/Sales.tsx
@@ -300,7 +300,7 @@ const handleCustomerLookup = async () => {
       <div className="md:w-2/3 bg-white rounded-lg shadow-sm p-4">
         <div className="mb-6">
           <div className="relative">
-            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={20} />
+            <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={10} />
             <input
               type="text"
               placeholder="Entrez le nom de l'article ou son barcode..."
