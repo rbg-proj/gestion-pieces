d42bf4e40aa96eb097593af4b017b9412d1369bb
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-11-22 11:24:43 +0100

    Updated Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -417,7 +417,7 @@ return (
           }}
           className="border rounded-md px-3 py-2 bg-gray-100"
         >
-          Réinitialiser
+          Réinitialiser Stock
         </button>
       </div>
 
