e6ccfa033d86995370afd69f469c1712b8942185
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-11-22 11:57:08 +0100

    Updated Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -144,7 +144,7 @@ const Products: React.FC = () => {
           }
         
           if (existing && existing.length > 0) {
-            setToastMessage("Un produit portant ce nom existe déjà !");
+            setToastMessage("Un produit portant ce nom existe déjà, utilisez un autre nom !");
             
             // 🔥 Redonner le focus au champ du nom
               setTimeout(() => {
