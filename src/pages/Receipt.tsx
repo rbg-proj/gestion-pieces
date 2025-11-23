0fdae3fa85f912731591a4948a8c2f20c2e7f433
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-11-23 11:56:25 +0100

    Updated Receipt.tsx

--- src/pages/Receipt.tsx
+++ src/pages/Receipt.tsx
@@ -66,7 +66,6 @@ const Receipt = forwardRef<HTMLDivElement, ReceiptProps>(
           <p>TVA (0%) : {taxAmount.toFixed(2)}Fc</p>
           <p className="font-bold">Total TTC : {total.toLocaleString('fr-FR')} Fc</p>
         </div>
-       
         <p className="text-center text-xs mt-4">Merci pour votre achat !</p>
       </div>
     );
