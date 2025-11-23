be9563c2339d09867b8a702fac42822498d8f093
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-11-23 12:26:27 +0100

    Updated Orders.tsx

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -323,7 +323,6 @@ const filteredOrders = orders
 
           <div ref={printRef}>
             {saleItems.length > 0 && selectedOrderInfo && (
-              console.log("Exchange rate:", selectedOrderInfo.exchange_rate);
               <Receipt
                 cart={saleItems.map(item => ({
                   name: item.products?.name || 'Produit inconnu',
