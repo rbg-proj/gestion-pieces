ac342b5d05f33791b04660483a35267cd60214ce
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-11-23 12:19:10 +0100

    Updated Orders.tsx

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -60,6 +60,7 @@ export default function OrdersPage() {
           total: sale.total_amount,
           items: sale.sale_items?.length || 0,
           paymentMethod: sale.payment_method || "Inconnu",
+          exchange_rate: sale.exchange_rate ?? 1,   // 🔥 ajout important
           status:
           sale.payment_method === "cash" || sale.payment_method === "mobile_money"
               ? "Payé"
