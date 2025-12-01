2e67a318b651f939312aa284af6647a75ef2791c
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-12-01 18:20:05 +0100

    Updated Receipt.tsx

--- src/pages/Receipt.tsx
+++ src/pages/Receipt.tsx
@@ -20,18 +20,17 @@ const Receipt = forwardRef<HTMLDivElement, ReceiptProps>(
     const totalHT = total / (1 + TAX_RATE);
     const taxAmount = total - totalHT;
 
-  const formattedDate = (() => {
-  if (!date) return "";
-  const d = new Date(date);
-  const day = String(d.getDate()).padStart(2, "0");
-  const month = String(d.getMonth() + 1).padStart(2, "0");
-  const year = d.getFullYear();
-  const hours = String(d.getHours()).padStart(2, "0");
-  const minutes = String(d.getMinutes()).padStart(2, "0");
-  const seconds = String(d.getSeconds()).padStart(2, "0");
+  
 
-  return `${day}-${month}-${year} ${hours}:${minutes}:${seconds}`;
-})();
+  const formattedDate = new Date(date).toLocaleString("fr-FR", {
+  day: "2-digit",
+  month: "2-digit",
+  year: "numeric",
+  hour: "2-digit",
+  minute: "2-digit",
+  second: "2-digit",
+  hour12: false,
+  });
 
 
     return (
