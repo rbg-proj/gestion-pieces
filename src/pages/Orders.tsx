63be35d33ecfc31927777976ee083f8c9024e629
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-12-01 17:34:31 +0100

    Updated Orders.tsx

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -347,7 +347,7 @@ const filteredOrders = orders
               //  userName={sale.profiles.full_name}     // ⬅️ L’agent correct !
                 exchangeRate={selectedOrderInfo.exchange_rate} 
                 
-                invoiceNumber={`Dupl FAC-${String(new Date().getFullYear()).slice(2)}${String(new Date().getMonth() + 1).padStart(2, '0')}-${String(selectedOrderId ?? '').slice(0, 6).toUpperCase()}`}
+                invoiceNumber={`Dupli Fac-${String(new Date().getFullYear()).slice(2)}${String(new Date().getMonth() + 1).padStart(2, '0')}-${String(selectedOrderId ?? '').slice(0, 6).toUpperCase()}`}
               />
             )}
           </div>
