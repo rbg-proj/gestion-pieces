62af3dabe6abf630062140adc337cfbe8cfcb86b
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-12-03 00:12:17 +0100

    Updated Orders.tsx

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -437,7 +437,7 @@ export default function OrdersPage() {
 
 
        {/* Sale Details Modal */}
-       {selectedSale && !isFormOpen && (
+       {selectedSale &&  (
                 <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
                   <div className="bg-white rounded-lg max-w-2xl w-full p-6 shadow-lg animate-fadeIn">
                     
