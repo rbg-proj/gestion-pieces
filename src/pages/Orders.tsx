80a2a5aba0cc311da4c6e7a7063f7f4489674948
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-12-02 16:18:36 +0100

    Updated Orders.tsx

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -114,11 +114,7 @@ export default function OrdersPage() {
     agent: string; 
   } | null>(null);
 
-  const todayLocal = new Date();
-  todayLocal.setHours(0, 0, 0, 0);
 
-  // → format YYYY-MM-DD en LOCAL
-  const todayISO = todayLocal.toLocaleDateString("fr-CA");
   
   const filteredOrders = orders
   .filter((order) => {
