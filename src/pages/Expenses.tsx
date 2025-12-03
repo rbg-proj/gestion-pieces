b138e9625fe69ff7699e3f42a223b879278135c3
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-12-03 15:22:34 +0100

    Updated Expenses.tsx

--- src/pages/Expenses.tsx
+++ src/pages/Expenses.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { supabase } from "../supabaseClient";
+import { supabase } from '../lib/supabase';
 import { Pencil, Trash2, PlusCircle } from "lucide-react";
 
 interface Expense {
