a52543ca44b424b59e863babf3386cd6c7ab5b4c
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-12-03 23:03:36 +0100

    Updated Expenses.tsx

--- src/pages/Expenses.tsx
+++ src/pages/Expenses.tsx
@@ -46,7 +46,6 @@ export default function Expenses() {
   const [showCategoryManager, setShowCategoryManager] = useState(false);
   
 
-
   useEffect(() => {
     fetchRole();
     fetchCategories();
