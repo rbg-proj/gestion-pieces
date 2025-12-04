346df80e42c024137e36838f43c3008afb187f2b
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-12-04 21:09:28 +0100

    Updated Expenses.tsx

--- src/pages/Expenses.tsx
+++ src/pages/Expenses.tsx
@@ -270,7 +270,7 @@ export default function Expenses() {
         onClick={exportExcel}
         className="px-4 py-2 bg-green-600 text-white rounded mb-4 hover:bg-green-700 transition"
       >
-        📤 Exporter Excel
+      Export Excel
       </button>
 
       {/* ===== TABLE ===== */}
