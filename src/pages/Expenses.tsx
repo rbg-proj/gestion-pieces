124dcad809f789ba46e1ef6f0926e824089383fe
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-12-04 21:14:14 +0100

    Updated Expenses.tsx

--- src/pages/Expenses.tsx
+++ src/pages/Expenses.tsx
@@ -265,13 +265,6 @@ export default function Expenses() {
         </div>
       </div>
 
-      {/* EXPORT */}
-      <button
-        onClick={exportExcel}
-        className="px-4 py-2 bg-green-600 text-white rounded mb-4 hover:bg-green-700 transition"
-      >
-      <FileSpreadsheet size={18} />Export Excel
-      </button>
 
       {/* ===== TABLE ===== */}
       <div className="overflow-x-auto shadow rounded-lg border border-gray-200">
