53c35314b8843d77c7d12c5f6cd9f278a2cdb5cc
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-12-05 16:42:52 +0100

    Updated SalesChart.tsx

--- src/components/dashboard/SalesChart.tsx
+++ src/components/dashboard/SalesChart.tsx
@@ -25,8 +25,8 @@ const formatCurrency = (value: number) => {
   return new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD',
-    minimumFractionDigits: 0,
-    maximumFractionDigits: 0,
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
   }).format(value);
 };
 
