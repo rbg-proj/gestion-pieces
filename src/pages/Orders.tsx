4d48b6d23ed1ca808f3238191bb70570df6e3078
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-12-11 13:16:09 +0100

    Updated Orders.tsx

--- src/pages/Orders.tsx
+++ src/pages/Orders.tsx
@@ -449,7 +449,7 @@ export default function OrdersPage() {
 
 
       {editModalOpen && editOrder && (
-  <div className="max-h-[80vh] bg-black bg-opacity-50 flex items-center justify-center overflow-y-auto p-4 z-50">
+  <div className="fixed inset-0  bg-black bg-opacity-50 flex items-center justify-center overflow-y-auto p-4 z-50">
     <div className="bg-white rounded-lg w-full max-w-3xl p-6 shadow-lg">
 
       {/* Header */}
