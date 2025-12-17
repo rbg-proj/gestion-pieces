a42fad710a9e9dc1aca0857be77e3a747d824035
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-12-17 23:29:28 +0100

    Updated StockMovementHistory.tsx

--- src/pages/StockMovementHistory.tsx
+++ src/pages/StockMovementHistory.tsx
@@ -145,7 +145,6 @@ export default function StockMovementsHistory() {
                       setQuery(p.name);
                     }}
                   >
-                    {p.name} — Stock: {p.stock}
                   </div>
                 ))}
               </div>
