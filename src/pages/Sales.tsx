52a1f586a46eeecd6df911a58440e3423177b518
rbg-proj <rehobothbusinessgroup1@gmail.com>
2025-12-26 11:18:35 +0100

    Updated Sales.tsx

--- src/pages/Sales.tsx
+++ src/pages/Sales.tsx
@@ -727,7 +727,6 @@ const Sales: React.FC = () => {
               total={printedTotal} // en CDF
               customerName={printedCustomerName}
               paymentMethod={printedPaymentMethod}
-              //date={new Date().toLocaleString()}
               date={new Date().toISOString()}
               invoiceNumber={generateInvoiceNumber(selectedSaleId)}
               userName={user?.name || ''}
