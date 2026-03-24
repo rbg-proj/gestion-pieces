54b84dc833a9c19e29706c5d4e1c03d6745b5ad6
rbg-proj <rehobothbusinessgroup1@gmail.com>
2026-03-24 11:03:53 +0100

    Updated Products.tsx

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -302,7 +302,7 @@ const Products: React.FC = () => {
     const fileName = `${Date.now()}.${fileExt}`;
     const filePath = `products/${fileName}`;
 
-    const { error } = await supabase.storage.from('product-images').upload(filePath, file);
+    const { error } = await supabase.storage.from('images-articles').upload(filePath, file);
     if (error) {
       console.error('Erreur upload :', error.message);
       toast.error("Échec de l’upload de l’image !");
